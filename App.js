0afd1d7cc4b09867b8563d20bd0febc6e9bcc10f
marwen22462 <58691682+marwen22462@users.noreply.github.com>
2020-02-11 14:40:40 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Time from "./component/time";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Time />
+      </div>
+    );
+  }
+}
+
+export default App;
